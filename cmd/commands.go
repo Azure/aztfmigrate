780182b72a59de7e2af8f9b273301d65f9e4068b
Heng Lu <henglu@microsoft.com>
2021-12-20 15:35:25 +0800

    use standard cli commands framework

---
+++ cmd/commands.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"flag"
+	"io/ioutil"
+	"strings"
+)
+
+func defaultFlagSet(cmdName string) *flag.FlagSet {
+	f := flag.NewFlagSet(cmdName, flag.ContinueOnError)
+	f.SetOutput(ioutil.Discard)
+
+	// Set the default Usage to empty
+	f.Usage = func() {}
+
+	return f
+}
+
+func helpForFlags(fs *flag.FlagSet) string {
+	buf := &strings.Builder{}
+	buf.WriteString("Options:\n\n")
+
+	w := fs.Output()
+	defer fs.SetOutput(w)
+	fs.SetOutput(buf)
+	fs.PrintDefaults()
+
+	return buf.String()
+}
---
+++ version.go
@@ -0,0 +1,5 @@
+package main
+
+func VersionString() string {
+	return "0.0.1"
+}
